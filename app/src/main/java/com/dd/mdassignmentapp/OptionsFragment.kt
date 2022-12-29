e6098926fe7255b4978941d0839148b9922d654c
darshanWork <87642358+darshanPersonal@users.noreply.github.com>
2022-12-29 18:19:55 +0000

    Basic Design for Game

--- app/src/main/java/com/dd/mdassignmentapp/OptionsFragment.kt
+++ app/src/main/java/com/dd/mdassignmentapp/OptionsFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import com.dd.mdassignmentapp.databinding.FragmentMainBinding
+import com.dd.mdassignmentapp.databinding.FragmentOptionsBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,14 +22,25 @@ private const val ARG_PARAM2 = "param2"
  */
 class OptionsFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding : FragmentOptionsBinding
+    private lateinit var navController : NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            //param1 = it.getString(ARG_PARAM1)
+            //param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = findNavController()
+        binding.earthButton.setOnClickListener{
+            navController.navigate(R.id.action_optionsFragment_to_earthFragment)
+        }
+        binding.moonButton.setOnClickListener{
+            navController.navigate(R.id.action_optionsFragment_to_moonFragment)
         }
     }
 
@@ -34,7 +49,10 @@ class OptionsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_options, container, false)
+        //return inflater.inflate(R.layout.fragment_options, container, false)
+
+        binding = FragmentOptionsBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     companion object {
